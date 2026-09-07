ca720d2bc5b0dc7984f4f5f11f517bb7a9be38b8
Divyansh Kumar <divyanshkrit@gmail.com>
2026-09-07 22:32:46 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> count;
+        for(int i = 0; i<s.length();i++){
+            count[s[i]]++;
+        }
+        for(int i = 0; i<t.length();i++){
+            count[t[i]]--;
+        }
+        for(auto i:count){
+            if(i.second != 0)
+                return false;
+        }
+        return true;
+    }
+};
